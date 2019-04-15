cee5a96a6e5310253e63492cb27b49c9b68f89ed
David Chen <david.chen@langlive.com>
2019-04-15 17:02:19 +0800

    init

--- app/src/main/java/com/example/mypaging/main/MainActivity.kt
+++ app/src/main/java/com/example/mypaging/main/MainActivity.kt
@@ -2,21 +2,25 @@ package com.example.mypaging.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.example.mypaging.R
+import com.example.mypaging.databinding.ActivityMainBinding
 import dagger.android.support.DaggerAppCompatActivity
 import javax.inject.Inject
 
 class MainActivity : DaggerAppCompatActivity(), MainView {
     @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
     private lateinit var viewModel: MainViewModel
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         viewModel = ViewModelProviders.of(this, viewModelFactory).get(MainViewModel::class.java)
+        binding.viewModel = viewModel
+
         viewModel.fetchPosts()
     }
 }
