8655c3e0fdf98939789fca72bff55b8645e08d4b
David Chen <david.chen@langlive.com>
2019-04-23 12:21:07 +0800

    remove epoxy dependency

--- app/src/main/java/com/example/mypaging/App.kt
+++ app/src/main/java/com/example/mypaging/App.kt
@@ -1,8 +1,5 @@
 package com.example.mypaging
 
-import android.os.Handler
-import android.os.HandlerThread
-import com.airbnb.epoxy.EpoxyController
 import com.example.mypaging.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
@@ -13,11 +10,6 @@ class App: DaggerApplication() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
-        val handlerThread = HandlerThread("epoxy").apply { start() }
-        Handler(handlerThread.looper).also {
-            EpoxyController.defaultDiffingHandler = it
-            EpoxyController.defaultModelBuildingHandler = it
-        }
     }
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
--- app/src/main/java/com/example/mypaging/main/MainActivity.kt
+++ app/src/main/java/com/example/mypaging/main/MainActivity.kt
@@ -5,9 +5,10 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mypaging.R
 import com.example.mypaging.databinding.ActivityMainBinding
-import com.example.mypaging.main.adapter.ArticleController
+import com.example.mypaging.main.adapter.ArticleAdapter
 import com.example.mypaging.main.data.NetworkState
 import dagger.android.support.DaggerAppCompatActivity
 import timber.log.Timber
@@ -24,21 +25,17 @@ class MainActivity : DaggerAppCompatActivity(), MainView {
         viewModel = ViewModelProviders.of(this, viewModelFactory).get(MainViewModel::class.java)
         binding.viewModel = viewModel
 
-        // setup list adapter/controller
-//        binding.rvPosts.layoutManager = LinearLayoutManager(this)
-//        binding.rvPosts.adapter = ArticleAdapter { viewModel.retry() }
-        val controller = ArticleController { viewModel.retry() }
-        binding.rvPosts.setController(controller)
+        // setup page list adapter
+        binding.rvPosts.layoutManager = LinearLayoutManager(this)
+        binding.rvPosts.adapter = ArticleAdapter { viewModel.retry() }
         Timber.d("MainActivity onCreate")
         viewModel.articleLivedata.observe(this, Observer {
             Timber.d("observe list, size = ${it.size}")
-            controller.submitList(it)
-//            (binding.rvPosts.adapter as ArticleAdapter).submitList(it)
+            (binding.rvPosts.adapter as ArticleAdapter).submitList(it)
         })
         viewModel.networkState.observe(this, Observer {
             Timber.d("observe networkState, it = $it")
-            controller.networkState = it
-//            (binding.rvPosts.adapter as ArticleAdapter).setNetworkState(it)
+            (binding.rvPosts.adapter as ArticleAdapter).setNetworkState(it)
         })
         viewModel.refreshState.observe(this, Observer {
             Timber.d("observe refreshState, it = $it")
